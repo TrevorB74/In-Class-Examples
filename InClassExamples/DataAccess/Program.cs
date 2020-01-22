48320ace324d9c18446321ddb87f5a8b43da5f5f
Trevor Bryant <Trevor.Bryant-1@ou.edu>
2020-01-22 11:09:47 -0600

    created initial solution for in class examples and created project

---
+++ InClassExamples/DataAccess/Program.cs
@@ -0,0 +1,26 @@
+﻿// Trevor Bryant
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
