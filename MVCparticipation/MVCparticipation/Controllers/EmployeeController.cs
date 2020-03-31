6e05f0d8e1f4859e8ead167f0b51608927059e0d
TrevorB74 <46358194+TrevorB74@users.noreply.github.com>
2020-03-30 23:50:39 -0500

    finished participation

---
+++ MVCparticipation/MVCparticipation/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCparticipation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+
+            EmployeeController info = new EmployeeController();
+            
+
+            return View();
+        }
+
+        public IActionResult EmployeeInfo()
+        {
+
+            ViewBag.message = "I referenced the CDN bootstrap in my html but im not exactly sure what I am doing";
+
+            return View();
+
+        }
+
+    }
+}
---
+++ MVCparticipation/MVCparticipation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCparticipation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
